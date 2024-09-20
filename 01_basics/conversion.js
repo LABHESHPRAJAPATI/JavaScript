fe6b36149a7fd0824ab03c6c1da374e0d12bbfff
LABHESH PRAJAPATI <labheshprajapatilucky@gmail.com>
2024-09-20 11:46:39 +0000

    datatypes & conversion

---
+++ 01_basics/conversion.js
@@ -0,0 +1,17 @@
+let age= "labhesh"+5
+
+let Name=Number(age)
+let str=String(age)
+let bol=Boolean(age)
+let date = Date(age)
+
+
+
+
+console.log(typeof age,age);
+console.log(typeof Name,Name);
+console.log(typeof str,str);
+console.log(typeof bol,bol);
+console.log(typeof date,date);
+
+
---
+++ 01_basics/datatypes.js
@@ -0,0 +1,19 @@
+let Name="labhesh"
+let age=21
+let money=BigInt('1234567890000000000000000000000000000000000')
+let city
+let degree=null
+ let address={
+    NO:12,
+    town:"surat"
+}
+
+console.table([Name,age,city,
+    money,address,
+]);
+console.log(typeof Name);
+console.log(typeof age);
+console.log(typeof city);
+console.log(typeof money);
+console.log(typeof address);
+console.log(typeof degree);
