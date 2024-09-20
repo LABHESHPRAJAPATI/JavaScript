5543e4acad335a25d01779fd2c21528a1de69396
LABHESH PRAJAPATI <labheshprajapatilucky@gmail.com>
2024-09-20 10:02:23 +0000

    variables

---
+++ 01_basics/variables.js
@@ -0,0 +1,21 @@
+const Id=123
+var Name="labhesh prajapati"
+let Email="labhesh@gmail.com"
+city='surat'
+
+let name='labhesh', town='sirodi', age=21
+
+
+var Name="sohan "
+// let Email="sohan@gmail.com"  not redeclared
+city='sirohi'
+
+// // Id=1234
+// Name="labhesh kumar"
+// Email="labhesh@yahoo.com"
+
+let value = 1+3+7+"00"
+
+console.log(value)
+console.table([Id,Name,Email,city])
+console.table([,name,town,age])
