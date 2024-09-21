8d603be0058a444cb976727de0140a17ff0b56a5
LABHESH PRAJAPATI <labheshprajapatilucky@gmail.com>
2024-09-21 09:08:29 +0000

    stack and heap memory management

---
+++ 01_basics/stack_heap.js
@@ -0,0 +1,34 @@
+// stack memory for  primitive datatypes 
+
+
+let person ="rakesh"
+let unknowPerson= person
+
+console.log(unknowPerson);
+
+// heap  memory for  primitive datatypes 
+
+const user={
+    name :"labhesh",
+    age:21
+
+}
+
+const userTwo=user
+
+userTwo.name="paras"
+
+console.log(userTwo.name);
+console.log(user.name);
+
+const number=[1,2,3,4,5]
+const numberTwo=number
+
+numberTwo.push(12)
+number.pop()
+numberTwo.reverse()
+
+console.log(number);
+console.log(numberTwo);
+
+
