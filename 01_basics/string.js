2f428fabc28d87a67e5502a3852dd77f0e562306
LABHESH PRAJAPATI <labheshprajapatilucky@gmail.com>
2024-09-21 17:39:12 +0000

    string and his mathods and properties

---
+++ 01_basics/string.js
@@ -0,0 +1,67 @@
+let firstName=".      LABHESH.      "
+let lastName="prajapati"
+let x="hello ,world,hello sir, hello,surat ,and surat is beautiful Hello"
+
+let city = 'surat'
+
+// console.log(firstName.concat(" ",lastName));
+// console.log(firstName.substring(3,7));
+// console.log(lastName.charCodeAt(3));
+// console.log(lastName.codePointAt(2));
+// console.log(lastName.endsWith('i'));
+// console.log(lastName.includes('pa'));
+// console.log(lastName.indexOf('a'));
+// console.log(firstName.lastIndexOf('h',5));
+// console.log(lastName.length);
+// console.log(firstName.localeCompare(lastName))
+// console.log(x.match(/hello/gi));
+// console.log(x.matchAll(/hello/g));
+// console.log(firstName.padEnd(5,"0"));
+// console.log(firstName.padStart(4,'pl'));
+// console.log(firstName.repeat(10));
+// console.log(x.replace(/hello/gi,"jay ram"));
+// console.log(x.replaceAll('hello','jay ram'));
+// console.log(x.search('surat'));
+// console.log(x.slice(2,-15));
+// console.log(x.split(","));
+// console.log(x.startsWith('h'));
+// console.log(x.substring(6,12));
+// console.log(firstName.toLocaleLowerCase(2));
+// console.log(x.toLocaleUpperCase());
+// console.log(firstName.trimStart());
+// console.log(x.valueOf());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+console.log(`my name is ${firstName} and i live in ${city}`);
+
