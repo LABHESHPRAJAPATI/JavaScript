718b1dc47e8107411fbdf688ea5e0ec79c2a0bdc
LABHESH PRAJAPATI <labheshprajapatilucky@gmail.com>
2024-09-24 17:36:15 +0000

    arrays

---
+++ 02_basics/Arrays.js
@@ -0,0 +1,55 @@
+const arr=["labhesh","rakesh","paras","prakash","bhavin"]
+const arr1=[1,2,3,4,5]
+const points = [40, 100, 1, 5, 25, 10];
+
+// console.log(arr.shift());
+// console.log(arr.unshift('yogesh'));
+// console.log(arr.concat(arr1,arr1));
+// console.log(arr.copyWithin(2,0));
+// console.log(arr1.flat(Infinity));
+// console.log(arr1.flatMap((value)=>(value*2)));
+//  const arr2=arr.splice(3,0,"sohan")
+// console.log(arr);
+// console.log(arr.slice(2,4));
+// console.log(arr.indexOf("prakash"));
+// console.log(arr.lastIndexOf("rakesh")+1);
+// console.log(arr.includes("labhesh"));
+// console.log(arr1.find(display));
+//  function display(x){
+//  return x > 3
+//  }
+//  console.log(arr1.findIndex(display));
+//  function display(x){
+//  return x > 3
+//  }
+
+// console.log(arr1.findLast(x=>x>3));
+// console.log(arr1.findLastIndex(x=>x>3));
+// console.log(arr.sort());
+// console.log(arr1.sort());
+// console.log(arr.reverse());
+// console.log(arr1.reverse());
+// const points = [40, 100, 1, 5, 25, 10];
+// console.log(points.sort((x,y)=>(x-y)));
+// console.log(points.sort(()=>(0.5-Math.random())));
+
+// for (let i = points.length -1; i > 0; i--) {
+//     let j = Math.floor(Math.random() * (i+1));
+//     let k = points[i];
+//     points[i] = points[j];
+//     console.log(points[j] = k);
+    
+//   }
+
+
+
+
+
+
+
+
+
+
+
+
+
