e4fbe42411f59360bb85ba50f556127425006c85
LABHESH PRAJAPATI <labheshprajapatilucky@gmail.com>
2024-09-25 16:36:56 +0000

    function

---
+++ 03_basics/Function.js
@@ -0,0 +1,33 @@
+// function sayMyName(name="labhesh"){
+//     console.log(`my name is:${name}`);
+    
+// }
+
+// sayMyName("rakesh")
+
+// function addTwoNumber(num1,num2){
+
+//     return num1+num2
+// }
+// console.log(addTwoNumber(6,4));
+
+// function addCartItemsPrice(...num){
+//     return num
+// }
+// let cartPrice=addCartItemsPrice(200,300,400,500,600)
+// console.log(cartPrice);
+
+// function myArray(myArray){
+// return myArray[2]
+// }
+
+// console.log(myArray([100,20,40,50]));
+
+
+// function myObject(obj){
+// return obj.name
+// }
+//  console.log(myObject({name:"labhesh",
+//     age:21
+// }));
+ 
