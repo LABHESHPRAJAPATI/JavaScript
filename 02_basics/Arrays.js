fb2cc6c4b728159e76d2233ba3d8e02d1884b038
LABHESH PRAJAPATI <labheshprajapatilucky@gmail.com>
2024-09-25 09:43:33 +0000

    Arrays iterations

--- 02_basics/Arrays.js
+++ 02_basics/Arrays.js
@@ -38,8 +38,121 @@ const points = [40, 100, 1, 5, 25, 10];
 //     let k = points[i];
 //     points[i] = points[j];
 //     console.log(points[j] = k);
+//   }
+//  console.log(points.sort((a,b)=>(a-b)));
+//  console.log(points[0]);
+//  console.log(points[points.length-1]);
+ 
+// function arr_Min(arr){
+//     // return Math.min(...points)
+//     return Math.min.apply(null,points)
+// }
+// function arr_Max(arr){
+//     // return Math.max.apply(null,arr)
+//     return Math.max(...points)
+// }
+ 
+// console.log(arr_Max(points));
+// console.log(arr_Min(points));
+
+// function arr_Min1(arr){
+//   let length=arr.length
+//   let min=Infinity
+//   while (length--) {
+//     if (arr[length]<min) {
+//         min=arr[length]
+//     }
     
 //   }
+//   return min
+// }
+
+
+// function arr_Max1(arr){
+//     let length=arr.length
+//     let max=-Infinity
+//     while (length--) {
+//       if (arr[length]>max) {
+//           max=arr[length]
+//       }
+      
+//     }
+//     return max
+//   }
+// console.log( arr_Min1(points));
+// console.log( arr_Max1(points));
+
+// const cars = [
+//     {type:"Volvo", year:2016},
+//     {type:"Saab", year:2001},
+//     {type:"BMW", year:2010}
+//   ];
+
+// cars.sort((a,b)=>(a.year-b.year))
+//  console.log(`type:${cars[0].type}  year:${cars[0].year}`);
+//  console.log(`type:${cars[1].type}  year:${cars[1].year}`);
+//  console.log(`type:${cars[2].type}  year:${cars[2].year}`);
+
+
+
+//   ****loops for arrays ******
+
+
+// arr.forEach((value,index,arr)=>console.log(`${value}  ${index}.  ${arr}`))
+// newArr=points.map((X)=>(  X=X*2))
+// console.log(newArr);
+// arr2=points.flatMap((a)=>(a*2))
+
+// console.log(arr2);
+
+// const arr3=points.filter((a)=>(a>18))
+// console.log(arr3);
+
+// const arr4=points.reduce((x,a)=>(x+=a))
+// console.log(arr4);
+
+// const arr4=points.reduceRight((x,a)=>(x+=a))
+// console.log(arr4);
+
+
+// const arr4=points.every((x)=>(x<=100))
+// console.log(arr4);
+
+// const arr4=points.some((x)=>(x==10))
+// console.log(arr4);
+
+// console.log(Array.from("labhesh"));
+// const me=Array.from("labhesh")
+// console.log(me[4]);
+
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+
+// let x=fruits.keys()
+// for (const element of x) {
+//     console.log(element);
+    
+// }
+
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+
+// let x=fruits.entries()
+// for (const element of x) {
+//     console.log(element);
+    
+// }
+
+// const q1 = ["Jan", "Feb", "Mar"];
+// const q2 = ["Apr", "May", "Jun"];
+// const q3 = ["Jul", "Aug", "Sep"];
+// const q4 = ["Oct", "Nov", "May"];
+
+// const month=[...q1,...q2,...q3,...q4]
+
+// console.log(month);
+
+
+
+
 
 
 
