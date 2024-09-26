bc69c4e18f5422774b0d082e3dc5aaaaac41c1d7
LABHESH PRAJAPATI <labheshprajapatilucky@gmail.com>
2024-09-26 09:48:18 +0000

    IIFE function

---
+++ 03_basics/IIFE_function.js
@@ -0,0 +1,10 @@
+// (function name(name){
+//     console.log(`name is${name}`);
+    
+// })('labhesh');
+// name("labhesh")
+
+// ((name)=>{
+//     console.log(`name is ${name}`);
+    
+// })("rakesh")
--- 03_basics/Scope.js
+++ 03_basics/Scope.js
@@ -1,21 +1,21 @@
-// // const name="labhesh"
-// // let age=22
-// // var city="surat"
+const name="labhesh"
+let age=22
+var city="surat"
 
 
 
 
-// function display(){
-//     const name="rakesh"
-//     let age=21
-//     var city="ahm"
-//     console.log(name);
-//     console.log(age);
-//     console.log(city);
+function display(){
+    const name="rakesh"
+    let age=21
+    var city="ahm"
+    console.log(name);
+    console.log(age);
+    console.log(city);
     
     
-// }
-// // console.log(name);
+}
+// console.log(name);
 // // console.log(age);
 // console.log(city);
 // display()
