9cf1636cd40d4746b7b98c3474124c4a7a7ad9ef
LABHESH PRAJAPATI <labheshprajapatilucky@gmail.com>
2024-09-26 09:20:18 +0000

    scope and Arrorfunction &this keyword

---
+++ 03_basics/ArrorFunction.js
@@ -0,0 +1,31 @@
+// const person={
+//     name:"labhesh",
+//     age:22,
+//     display:function(){
+//         console.log(`welcome to website:${this.name}`);
+//         // console.log(this);
+        
+        
+//     }
+// }
+
+
+// function display() {
+//     console.log(this);
+// }
+// console.log(this);
+// display()
+// person.display()
+
+// const name=()=>{
+//     console.log(this)
+// }
+//  name()
+
+
+// const fun1=()=> console.log("hello everyone");
+
+// fun1()
+ 
+// const add=(a,b)=>(a+b)
+// console.log(add(8,9))
---
+++ 03_basics/Scope.js
@@ -0,0 +1,35 @@
+// // const name="labhesh"
+// // let age=22
+// // var city="surat"
+
+
+
+
+// function display(){
+//     const name="rakesh"
+//     let age=21
+//     var city="ahm"
+//     console.log(name);
+//     console.log(age);
+//     console.log(city);
+    
+    
+// }
+// // console.log(name);
+// // console.log(age);
+// console.log(city);
+// display()
+function myfun() {
+    let name ="sohan"
+
+    function myfun1() {
+        let age=22
+        console.log(name);
+        
+    }
+    // console.log(age);
+    
+    myfun1()
+    
+}
+myfun()
