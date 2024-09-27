31cb4100cde1ce6beba0f89037dabd4913cbf2c0
LABHESH PRAJAPATI <labheshprajapatilucky@gmail.com>
2024-09-27 09:08:25 +0000

    control  flow if and switch case

---
+++ 04_control_flow/if_else.js
@@ -0,0 +1,66 @@
+// if (true) {
+//     console.log("heyy my anme is labhesh kumar");
+    
+// }   
+// let score=19
+// if (score==18) {
+//     console.log(`hey your score is 18`);
+    
+// } else {
+//     console.log(`hey your score  is less than 18`);
+// }
+
+let days=new Date().getDay()
+// console.log(days);
+
+// Monday, Tuesday, Wednesday, Thursday, Friday, Saturday and Sunday.
+//  if (days==0) {
+//     console.log("sunday");
+    
+// } else if (days==1) {
+//     console.log("monday");
+// } else if (days==2) {
+//     console.log("tuesday");
+// } else if (days==3) {
+//     console.log("wednesday");
+// } else if (days==4) {
+//     console.log("thursday");
+// } else if (days=="5") {
+//     console.log("friday");
+// } else if (days==6) {
+//     console.log("saturday");
+// } else {
+//     console.log("not match ");
+// }{
+    
+// }
+
+
+// switch (days+1) {
+//     case 1:
+//         console.log("sunday");
+//         break;
+//     case 2:
+//         console.log("monday");
+//         break;
+//     case 3:
+//         console.log("tuesday");
+//         break;
+//     case 4:
+//         console.log("wednesday");
+//         break;
+//     case 5:
+//         console.log("thusrday");
+//         break;
+//     case "6":
+//         console.log("friday");
+//         break;
+//     case 7:
+//         console.log("staturday");
+//         break;
+
+//     default:
+//         console.log("defult statement");
+
+//         break;
+// }
