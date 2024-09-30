586ab4cc58b89d90a8f82b7a57a9471c34226670
LABHESH PRAJAPATI <labheshprajapatilucky@gmail.com>
2024-09-30 08:08:44 +0000

    while and do while loops

---
+++ 05_loops/while_loops.js
@@ -0,0 +1,54 @@
+let x = 1;
+
+let z;
+
+// while (x<=20) {
+//     console.log(`value of x is:${x}`);
+//     x+=2
+// }
+
+// while (x <= 10) {
+//   let pattern = " ";
+//   let y = 1;
+
+//  while (y <= x) {
+//     pattern += " *";
+//     y++;
+//   }
+//   console.log(pattern);
+//   x++;
+// }
+
+// let n = 5; // Number of rows
+// let row = 1; // Starting row
+
+// while (row <= n) {
+//   let pattern = "  ";
+//   let col = 1;
+
+//   while (col <= row) {
+//     pattern += "*";
+//     col++;
+//   }
+
+//   console.log(pattern);
+//   row++;
+// }
+
+// do {
+//     console.log(x);
+//     x++
+// } while (x<=10);
+
+// do {
+//     pattern=""
+//     let y=1
+//     do {
+//        pattern+=" a" 
+//        y++
+//     } while (y<=x);
+
+//     console.log(pattern);
+//     x++
+    
+// } while (x<=5);
